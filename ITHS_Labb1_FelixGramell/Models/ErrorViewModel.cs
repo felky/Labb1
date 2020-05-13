621570c03be4150682a924aed72e707d7a957c2e
felky <felixg.nti@gmail.com>
2020-05-13 15:30:53 +0200

    Initial commit

---
+++ ITHS_Labb1_FelixGramell/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ITHS_Labb1_FelixGramell.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
