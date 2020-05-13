7f1c944b327a21eaa94091ebb18490f264ca92ba
felky <felixg.nti@gmail.com>
2020-05-13 23:10:58 +0200

    Added shopping cart

---
+++ ITHS_Labb1_FelixGramell/ViewModels/CartVM.cs
@@ -0,0 +1,13 @@
+﻿using ITHS_Labb1_FelixGramell.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ITHS_Labb1_FelixGramell.ViewModels
+{
+    public class CartVM
+    {
+        public ShoppingCart Cart { get; set; }
+    }
+}
